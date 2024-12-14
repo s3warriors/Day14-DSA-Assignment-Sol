06ff0551e581d263384694413f563e62b6d2aedc
s3warriors <satveerswami982@gmail.com>
2024-12-14 21:08:15 +0530

    Added: Solution of Merge Sort

---
+++ src/MergeSort.java
@@ -0,0 +1,52 @@
+public class MergeSort {
+    public static void mergeSort(String[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(String[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        String[] leftArr = new String[n1];
+        String[] rightArr = new String[n2];
+
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i].compareTo(rightArr[j]) <= 0) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = rightArr[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"apple", "orange", "banana", "grape", "cherry"};
+
+        mergeSort(words, 0, words.length - 1);
+
+        System.out.println("Sorted List:");
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
