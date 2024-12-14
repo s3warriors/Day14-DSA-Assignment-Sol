4e773708227a813b0811637025e09122819123c8
s3warriors <satveerswami982@gmail.com>
2024-12-14 21:07:38 +0530

    Added: Solution of Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of integers:");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+        System.out.println("Enter the integers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        bubbleSort(numbers);
+
+        System.out.println("Sorted List:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
