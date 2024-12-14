e486d65b0b3522c6980d49e7030fcbb0dc2ceb9e
s3warriors <satveerswami982@gmail.com>
2024-12-14 21:09:57 +0530

    Added: Solution of Anagram Detection

---
+++ src/AnagramDetection.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class AnagramDetection {
+    public static boolean areAnagrams(String str1, String str2) {
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "heart";
+        String str2 = "earth";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The two strings are anagrams.");
+        } else {
+            System.out.println("The two strings are not anagrams.");
+        }
+    }
+}
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -50,3 +50,5 @@ public class MergeSort {
         }
     }
 }
+
+
